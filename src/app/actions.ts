a3a8621d7b589dc2533cbe51dc46485a9ca2baca
Jonathan Xue <j52xue@uwaterloo.ca>
2024-05-06 18:46:21 -0400

    Initial commit

---
+++ src/app/actions.ts
@@ -0,0 +1,11 @@
+import { amplifyClient } from './amplify-utils'
+
+export async function generateCocktail(formData: FormData) {
+  const response = await amplifyClient.queries.askBedrock({
+    ingredients: [formData.get('ingredients')?.toString() || ''],
+  })
+
+  const res = JSON.parse(response.data?.body!)
+  const content = res.content[0].text
+  return content || ''
+}
